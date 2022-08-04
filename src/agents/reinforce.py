2efeec56b5ef0ac39adb8ab88cbe63e1d88a04aa
Matteo Conti <contimatteo1505@gmail.com>
2022-08-04 19:11:40 +0200

    minor changes

--- src/agents/reinforce.py
+++ src/agents/reinforce.py
@@ -1,35 +1,49 @@
 # from typing import Any
-
 # import numpy as np
 # import tensorflow as tf
-
 # from tensorflow.python.keras import Model, Sequential
-
 # from agents.baseline import Agent
+
+import gym
+
+from memory import SequentialMemory
+from __types import T_Action, T_State, T_Reward
 
 # ###
 
 # class REINFORCE(Agent):
-
 #     def __init__(self) -> None:
 #         super().__init__()
-
 #         self.policy_network = None
-
 #     #
-
 #     def _policy_network(self) -> Model:
 #         return Sequential()
-
 #     #
-
 #     def initialize(self) -> None:
 #         self.policy_network = self._policy_network()
-
 #     #
-
 #     def train(self):
 #         pass
-
 #     def test(self):
 #         pass
+
+
+class REINFORCE():
+
+    def __init__(self, n_max_episode_steps: int) -> None:
+        self.memory = SequentialMemory(n_max_episode_steps)
+
+    def act(self, env: gym.Env, state: T_State) -> T_Action:
+        return env.action_space.sample()
+
+    def remember(
+        self, step: int, state: T_State, action: T_Action, reward: T_Reward, next_state: T_State
+    ) -> None:
+        self.memory.store(
+            step, {
+                "state": state,
+                "action": action,
+                "reward": reward,
+                "next_state": next_state
+            }
+        )
--- src/reinforce.py
+++ src/reinforce.py
@@ -1,13 +1,9 @@
-from typing import Any, Tuple
-
 import time
-import numpy as np
 import gym
 
 from loguru import logger
 
-from memory import SequentialMemory
-from __types import T_Action, T_State, T_Reward
+from agents import REINFORCE
 
 ###
 
@@ -20,32 +16,8 @@ env = gym.make("CartPole-v1")
 ###
 
 
-class REINFORCE():
-
-    def __init__(self) -> None:
-        self.memory = SequentialMemory(N_MAX_EPISODE_STEPS)
-
-    def act(self, state: T_State) -> T_Action:
-        return env.action_space.sample()
-
-    def remember(
-        self, step: int, state: T_State, action: T_Action, reward: T_Reward, next_state: T_State
-    ) -> None:
-        self.memory.store(
-            step, {
-                "state": state,
-                "action": action,
-                "reward": reward,
-                "next_state": next_state
-            }
-        )
-
-
-###
-
-
 def main():
-    agent = REINFORCE()
+    agent = REINFORCE(N_MAX_EPISODE_STEPS)
 
     next_state = None
     state, _ = env.reset(seed=42, return_info=True)
@@ -57,7 +29,7 @@ def main():
         agent.memory.reset()
 
         for step in range(N_MAX_EPISODE_STEPS):
-            action = agent.act(state)
+            action = agent.act(env, state)
 
             next_state, reward, done, _ = env.step(action)
             env.render()
