MAJOR issue fixed (`gradients` computation returns `None`)

src/agents/ac.py
@@ -207,8 +207,8 @@ class AC(Agent):
         next_states_val = tf.reshape(next_states_val, (len(next_states_val)))
 
         ### Action Advantage Estimates
-        advantages = self._advantage_estimates(
-            rewards, disc_rewards, states_val, next_states_val, dones
+        advantages = tf.stop_gradient(
+            self._advantage_estimates(rewards, disc_rewards, states_val, next_states_val, dones)
         )
         advantages = self.__standardize_advantages(advantages)
 
@@ -270,16 +270,17 @@ class AC(Agent):
 
             with tf.GradientTape() as a_tape, tf.GradientTape() as c_tape:
                 _states_val = self.critic_network(_trajectories, training=True)
-                actions_probs = self.actor_network(_trajectories, training=True)
+                _actions_coeff = self.actor_network(_trajectories, training=True)
 
                 _states_val = tf.reshape(_states_val, (len(_states_val)))
+                _deltas = _advantages
+                # _deltas = tf.math.subtract(_advantages, _states_val)
 
-                deltas = advantages
-                # deltas = tf.math.subtract(_advantages, _states_val)
-
-                actor_loss = self._actor_network_loss(actions_probs, _actions, deltas)
+                actor_loss = self._actor_network_loss(
+                    _actions_coeff, _actions, tf.stop_gradient(_deltas)
+                )
                 critic_loss = self._critic_network_loss(
-                    _rewards, _disc_rewards, _advantages, _states_val
+                    _rewards, _disc_rewards, tf.stop_gradient(_advantages), _states_val
                 )
 
                 assert not tf.math.is_inf(actor_loss) and not tf.math.is_nan(actor_loss)

src/utils/action.py
@@ -11,10 +11,6 @@ from tensorflow_probability.python.distributions import Distribution
 
 ###
 
-PY_NUMERIC_EPS = 1e-8
-
-###
-
 
 class ActionUtils():
 
@@ -41,14 +37,15 @@ class ActionUtils():
         if discrete:
             assert coefficients.shape[0] == n_actions
 
-            probabilities = coefficients.numpy()
+            probabilities = coefficients
             distribution = tfp.distributions.Categorical(logits=probabilities, dtype=tf.float32)
         else:
             assert coefficients.shape[0] == n_actions * 2
 
             mu = coefficients[:n_actions]
-            sigma = coefficients[n_actions:] + PY_NUMERIC_EPS
-            # sigma = tf.nn.softplus(sigma) + 1e-5
+            sigma = coefficients[n_actions:]
+            # sigma = tf.nn.softplus(sigma)
+            # sigma += 1e-5
 
             assert mu.shape[0] == sigma.shape[0] == n_actions
 