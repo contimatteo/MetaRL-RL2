1dcb657644b14666b538848feda4769ee377f834
Matteo Conti <contimatteo1505@gmail.com>
2022-08-22 13:03:23 +0200

    changes

---
+++ scripts/render.all.sh
@@ -0,0 +1,30 @@
+
+# ### 1xCartPole
+# python src/run.py --config configs/render/1xCartPole/A2C.json
+# python src/run.py --config configs/render/1xCartPole/A3C.json
+# python src/run.py --config configs/render/1xCartPole/MetaA2C.json
+# python src/run.py --config configs/render/1xCartPole/MetaA3C.json
+
+# ### 1xQuadrupedal
+# python src/run.py --config configs/render/1xQuadrupedal/A2C.json
+# python src/run.py --config configs/render/1xQuadrupedal/A3C.json
+# python src/run.py --config configs/render/1xQuadrupedal/MetaA2C.json
+# python src/run.py --config configs/render/1xQuadrupedal/MetaA3C.json
+
+# ### 10xQuadrupedal
+# python src/run.py --config configs/render/10xQuadrupedal/A2C.json
+# python src/run.py --config configs/render/10xQuadrupedal/A3C.json
+# python src/run.py --config configs/render/10xQuadrupedal/MetaA2C.json
+# python src/run.py --config configs/render/10xQuadrupedal/MetaA3C.json
+
+# ### 1xLunarLander
+# python src/run.py --config configs/render/1xLunarLander/A2C.json
+# python src/run.py --config configs/render/1xLunarLander/A3C.json
+# python src/run.py --config configs/render/1xLunarLander/MetaA2C.json
+# python src/run.py --config configs/render/1xLunarLander/MetaA3C.json
+
+# ### 4xLunarLander
+# python src/run.py --config configs/render/4xLunarLander/A2C.json
+# python src/run.py --config configs/render/4xLunarLander/A3C.json
+# python src/run.py --config configs/render/4xLunarLander/MetaA2C.json
+# python src/run.py --config configs/render/4xLunarLander/MetaA3C.json
